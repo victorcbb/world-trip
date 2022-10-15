101d37776b0aeca079a13968c447ad35d32859d1
Victor Barros <victorclovisb@hotmail.com>
2022-10-14 21:03:33 -0300

    Implementing carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex, Heading } from "@chakra-ui/react"
 import { Banner } from "../componets/Banner";
+import { ContinentsSlide } from "../componets/ContinentsSlide";
 import { Header } from "../componets/Header";
 import { TravelType } from "../componets/TravelType";
 
@@ -21,6 +22,21 @@ export default function Home() {
         <TravelType image="museum.svg" type="clássico" />
         <TravelType image="earth.svg" type="e mais..." />
       </Flex>
+
+      <Box 
+        w="5.625rem" 
+        h={0} 
+        border="1px" 
+        borderColor="brand.texts" 
+        mx="auto" 
+        mt="20" 
+      />
+
+      <Heading as="h2" textAlign="center" fontWeight="medium" fontSize="4xl" m="3.25rem auto">
+        Vamos nessa?<br/> Então escolha seu continente
+      </Heading>
+
+      <ContinentsSlide />
     </Box>
   )
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -19,6 +19,57 @@ export const theme = extendTheme({
       body: {
         bg: "brand.bg",
         color: "brand.texts",
+      },
+
+      ".swiper": {
+        width: "100%",
+        maxWidth: "1240px",
+        height: "450px",
+        marginBottom: "4rem",
+      },
+      
+      ".swiper-slide": {
+        textAlign: "center",
+        fontSize: "18px",
+        bg: "#F5F8FA",
+        position: "realative",
+        color: "brand.bg",
+      
+
+        display: "flex",
+        "-webkit-box-pack": "center",
+        "-ms-flex-pack": "center",
+        "-webkit-justify-content": "center",
+        justifyContent: "center",
+        "-webkit-box-align": "center",
+        "-ms-flex-align": "center",
+        "-webkit-align-items": "center",
+        alignItems: "center",
+      },
+      
+      ".swiper-button-next, .swiper-button-prev": {
+        color: "brand.highlight",
+        margin: "0 1.25rem",
+      },
+      
+      ".swiper-slide img": {
+        display: "block",
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+        position: "absolute",
+        top: "0",
+        right: "0",
+        zIndex: "-1",
+      },
+
+      ".swiper-pagination-bullet": {
+        width: "1rem",
+        height: "1rem",
+      },
+
+      ".swiper-pagination-bullet-active": {
+        bg: "brand.highlight"
       }
     }
   }
